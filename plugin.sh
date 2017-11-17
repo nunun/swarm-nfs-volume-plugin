3d92965813ccbe0a3504666ed7f482ddc958f9ab
nunun <hoge.life@gmail.com>
2017-11-17 09:52:02 +0900

    ad sudo

--- plugin.sh
+++ plugin.sh
@@ -15,28 +15,28 @@ configure_exports_file() {
                 if [ ! -f "${EXPORTS_BACKUP_FILE}" ]; then
                         echo "${EXPORTS_FILE} already exists."
                         echo "backup to ${EXPORTS_BACKUP_FILE} ..."
-                        cp -v "${EXPORTS_FILE}" "${EXPORTS_BACKUP_FILE}"
+                        sudo cp -v "${EXPORTS_FILE}" "${EXPORTS_BACKUP_FILE}"
                 fi
         fi
-        mkdir -pv "${SWARM_NFS_PLUGIN_EXPORT_DIR}"
+        sudo mkdir -pv "${SWARM_NFS_PLUGIN_EXPORT_DIR}"
         echo "${SWARM_NFS_PLUGIN_EXPORT_DIR} ${EXPORTS}" > "${EXPORTS_FILE}"
 }
 
 on_install() {
         echo "swarm-nfs-plugin: on_install"
-        apt-get install nfs-server
+        sudo apt-get install nfs-server
         configure_exports_file
-        /etc/init.d/nfs-kernel-server start
+        sudo /etc/init.d/nfs-kernel-server start
 }
 
 on_uninstall() {
         echo "swarm-nfs-plugin: on_uninstall"
-        /etc/init.d/nfs-kernel-server stop
-        apt-get remove nfs-server
+        sudo /etc/init.d/nfs-kernel-server stop
+        sudo apt-get remove nfs-server
 }
 
 on_update() {
         echo "swarm-plugin-test: on_update"
         configure_exports_file
-        /etc/init.d/nfs-kernel-server restart
+        sudo /etc/init.d/nfs-kernel-server restart
 }
