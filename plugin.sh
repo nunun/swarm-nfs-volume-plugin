0b9440dfade9b996d152641a391d3fd385829517
nunun <hoge.life@gmail.com>
2017-11-28 01:26:19 +0900

    fix removing volume bug

--- plugin.sh
+++ plugin.sh
@@ -100,7 +100,7 @@ remove_nfs_volumes() {
         local host="${1}"
         local args="${host:+"arbitrary -H "}${host}"
         log_info "removing nfs volumes labelled '${SWARM_NFS_VOLUME_PLUGIN_VOLUME_LABEL}' ..."
-        swarm_pssh ${args} --inline "docker volume rm \`docker ls --filter=\"label=swarm-nfs-volume\"\`" \
+        swarm_pssh ${args} --inline "docker volume rm \`docker ls --f label=swarm-nfs-volume\`" \
                 > /tmp/swarm-nfs-volume-plugin.remove.log || log_info "done."
 }
 
