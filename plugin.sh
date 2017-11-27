244e4cfe111c42328d40186a93ea7849d045d2cf
nunun <hoge.life@gmail.com>
2017-11-28 01:53:34 +0900

    update plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -1,7 +1,7 @@
 SWARM_NFS_VOLUME_PLUGIN_NFS_SERVER_IP="${SWARM_NFS_VOLUME_PLUGIN_NFS_SERVER_IP:-"127.0.0.1"}"
 SWARM_NFS_VOLUME_PLUGIN_NFS_CLIENT_IP="${SWARM_NFS_VOLUME_PLUGIN_NFS_CLIENT_IP:-"127.0.0.1"}"
 SWARM_NFS_VOLUME_PLUGIN_NFS_EXTERNAL_VOLUMES="${SWARM_NFS_VOLUME_PLUGIN_NFS_EXTERNAL_VOLUMES:-"nfs_volume"}"
-SWARM_NFS_VOLUME_PLUGIN_EXPORT_DIR="${SWARM_NFS_VOLUME_PLUGIN_EXPORT_DIR:-"/exports/swarm-nfs-plugin"}"
+SWARM_NFS_VOLUME_PLUGIN_EXPORT_DIR="${SWARM_NFS_VOLUME_PLUGIN_EXPORT_DIR:-"/exports/swarm-nfs-volume-plugin"}"
 SWARM_NFS_VOLUME_PLUGIN_VOLUME_LABEL="${SWARM_NFS_VOLUME_PLUGIN_VOLUME_LABEL:-"swarm-nfs-volume"}"
 EXPORTS_FILE="/etc/exports"
 EXPORTS_BACKUP_FILE="/etc/exports.bak"
@@ -63,13 +63,15 @@ configure_exports_file() {
         if [ -z "${SWARM_NFS_VOLUME_PLUGIN_NFS_CLIENT_IP}" ]; then
                 abort "environment vairable 'SWARM_NFS_VOLUME_PLUGIN_NFS_CLIENT_IP' is empty."
         fi
-        EXPORTS=""
+        local exports=""
         for v in ${SWARM_NFS_VOLUME_PLUGIN_NFS_EXTERNAL_VOLUMES}; do
-                EXPORTS="${EXPORTS}${SWARM_NFS_VOLUME_PLUGIN_EXPORT_DIR}/${v}"
+                local export_dir="${SWARM_NFS_VOLUME_PLUGIN_EXPORT_DIR}/${v}"
+                exports="${exports}${export_dir}"
+                sudo mkdir -pv "${export_dir}"
                 for a in ${SWARM_NFS_VOLUME_PLUGIN_NFS_CLIENT_IP}; do
-                        EXPORTS="${EXPORTS} ${a}(rw,sync,no_subtree_check,no_root_squash)"
+                        exports="${exports} ${a}(rw,sync,no_subtree_check,no_root_squash)"
                 done
-                EXPORTS="${EXPORTS}\n"
+                exports="${exports}\n"
         done
         if [ -f "${EXPORTS_FILE}" ]; then
                 if [ ! -f "${EXPORTS_BACKUP_FILE}" ]; then
@@ -78,8 +80,7 @@ configure_exports_file() {
                         sudo cp -v "${EXPORTS_FILE}" "${EXPORTS_BACKUP_FILE}"
                 fi
         fi
-        sudo mkdir -pv "${SWARM_NFS_VOLUME_PLUGIN_EXPORT_DIR}"
-        echo -e ${EXPORTS} | sudo tee "${EXPORTS_FILE}"
+        echo -e ${exports} | sudo tee "${EXPORTS_FILE}"
 }
 
 create_nfs_volumes() {
@@ -104,52 +105,3 @@ remove_nfs_volumes() {
                 > /tmp/swarm-nfs-volume-plugin.remove.log || :
         log_info "done."
 }
-
-#local dir="${1}"
-#local compose_yml="${1}"
-#configure_compose_file "${dir}" "${compose_yml}"
-#configure_compose_file() {
-#        local dir="${1}"
-#        local compose_yml="${2}"
-#        local volumes_yml="${dir}/volumes.yml"
-#        local merging_yml="${dir}/merging.yml"
-#        local volumes=`docker-compose -f "${compose_yml}" config --volumes`
-#        local exports=""
-#
-#        # create volumes.yml
-#        echo "version: \"${SWARM_NFS_VOLUME_PLUGIN_COMPOSE_VERSION}\"" >  ${volumes_yml}
-#        echo "volumes: "                                        >> ${volumes_yml}
-#        for v in ${volumes}; do
-#                local found=""
-#                for f in ${SWARM_NFS_VOLUME_PLUGIN_NFS_EXTERNAL_VOLUMES}; do
-#                        if [ "${v}" = "${f}" ]; then
-#                                found="1"
-#                                break
-#                        fi
-#                done
-#                if [ -n "${found}" ]; then
-#                        echo "  ${v}:"                                            >> ${volumes_yml}
-#                        echo "    driver: local"                                  >> ${volumes_yml}
-#                        echo "    driver_opts:"                                   >> ${volumes_yml}
-#                        echo "      type: nfs4"                                   >> ${volumes_yml}
-#                        echo "      o: addr=${SWARM_NFS_VOLUME_PLUGIN_NFS_SERVER_IP},rw"     >> ${volumes_yml}
-#                        echo "      device: :${SWARM_NFS_VOLUME_PLUGIN_EXPORT_DIR}/${v}" >> ${volumes_yml}
-#
-#                        # NOTE
-#                        # remove directory manually if you want to uninstall.
-#                        sudo mkdir -p "${SWARM_NFS_VOLUME_PLUGIN_EXPORT_DIR}/${v}"
-#
-#                        # NOTE
-#                        # add to exports
-#                        exports="${exports} ${v}"
-#                fi
-#        done
-#
-#        # merge .current.yml and volumes.yml into .current.yml.
-#        if [ -n "${exports}" ]; then
-#                docker-compose -f "${compose_yml}" -f "${volumes_yml}" config > "${merging_yml}"
-#                cp "${merging_yml}" "${compose_yml}"
-#                log_info "nfs volumes are added to compose:${exports}"
-#                log_info "compose updated."
-#        fi
-#}
